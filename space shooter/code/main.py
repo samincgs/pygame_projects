916fe0c3a74015a2092f60ffd373d71e4b803677
samin <samincgs@gmail.com>
2024-06-28 17:35:27 -0400

    learneda bout keyboard and mouse input

--- space shooter/code/main.py
+++ space shooter/code/main.py
@@ -17,7 +17,7 @@ surf = pygame.Surface((100, 200))
 player_surf = pygame.image.load(join("images", "player.png")).convert_alpha()
 player_rect = player_surf.get_frect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 + 300))
 player_pos = pygame.Vector2(player_rect.center)
-player_direction = pygame.Vector2(1, -1)
+player_direction = pygame.Vector2()
 player_speed = 300
 
 meteor_surf = pygame.image.load(join("images", "meteor.png")).convert_alpha()
@@ -34,7 +34,23 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-            
+        # if event.type == pygame.KEYDOWN and event.key == pygame.K_1:
+        #     print("1")
+        # if event.type == pygame.MOUSEMOTION:
+        #     player_rect.center = event.pos
+    
+    # input
+    keys = pygame.key.get_pressed()
+    recent_keys = pygame.key.get_just_pressed()
+    
+    if recent_keys[pygame.K_SPACE]:
+        print("laser")
+    
+    player_direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
+    player_direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
+    player_direction = player_direction.normalize() if player_direction.magnitude() > 0 else player_direction
+    player_rect.center += player_direction * player_speed * dt
+    
     
     # draw the game
     display_surface.fill('darkgray')
@@ -50,13 +66,6 @@ while running:
     display_surface.blit(laser_surf, laser_rect)
     
     # player
-    
-    player_rect.center += player_direction * player_speed * dt
-    if player_rect.right > WINDOW_WIDTH or player_rect.left < 0:
-        player_direction.x *=-1
-    if player_rect.top < 0 or player_rect.bottom > WINDOW_HEIGHT:
-        player_direction.y *=-1
-        
     display_surface.blit(player_surf, player_rect)
     pygame.display.update()
 
