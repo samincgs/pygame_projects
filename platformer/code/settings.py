9e6da3cb3339d5b38d84601a62b8857c76ab6d59
samin <samincgs@gmail.com>
2024-08-07 17:41:15 -0400

    game complete

--- platformer/code/settings.py
+++ platformer/code/settings.py
@@ -1,8 +1,8 @@
 import pygame
-from os.path import join
+from random import randint, choice
 from pytmx.util_pygame import load_pygame
+from os.path import join
 from os import walk
-from random import choice, randint
 
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 TILE_SIZE = 64
